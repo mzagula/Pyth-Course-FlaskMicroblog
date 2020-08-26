0061c9a237633b7a141aa1f593b0d0b0f9510a81
Marta Zaguła <mzagula1992@gmail.com>
2020-08-26 19:52:26 +0200

    Add templates and forms

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author' : {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        {
+            'author' : {'username' : 'Suzane'},
+            'body' : 'nnfjkesnk'
+        }
 
+    ]
+    return render_template('index.html', title='Home', user=user, posts =posts)
