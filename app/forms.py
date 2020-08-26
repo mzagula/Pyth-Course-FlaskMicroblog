b5c4171ce41e9a6e3b6a60446a06976260c61967
Marta Zaguła <mzagula1992@gmail.com>
2020-08-26 22:33:03 +0200

    Validation while login/password is not filled

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import  DataRequired
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign in')
+    submit = SubmitField('Sign In')
