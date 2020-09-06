ec293883ee89be5c10933ff0b13128ffa8a5075f
Marta Zaguła <mzagula1992@gmail.com>
2020-09-06 20:33:10 +0200

    add follow and unfollow links in user profile links

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import  LoginForm,RegistrationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from flask_login import current_user, login_user
 from app.models import User
 from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
+
 
 @app.route('/')
 @app.route('/index')
@@ -23,7 +24,8 @@ def index():
     ]
     return render_template('index.html', title='Home', posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -40,12 +42,14 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -68,7 +72,9 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
+
 
 @app.before_request
 def before_request():
@@ -77,7 +83,7 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/edit_profile', methods=['GET','POST'])
+@app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
     form = EditProfileForm(current_user.username)
@@ -92,7 +98,8 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit profile', form=form)
 
-@app.route('/follow/<username>',methods=['POST'])
+
+@app.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
     form = EmptyForm()
@@ -103,30 +110,30 @@ def follow(username):
             return redirect(url_for('index'))
         if user == current_user:
             flash('You cannot follow yourself')
-            return redirect(url_for('user'), username = username)
+            return redirect(url_for('user'), username=username)
         current_user.follow(user)
         db.session.commit()
         flash('You are following {}'.format(username))
         return redirect(url_for('user', username=username))
-    else
+    else:
         return redirect(url_for('index'))
 
-@app.route('/unfollow/<username>', methods = ['POST'])
+
+@app.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
     form = EmptyForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=username).first()
         if user is None:
-            flash('User {} not found'.format(username))
+            flash('User {} not found.'.format(username))
             return redirect(url_for('index'))
         if user == current_user:
-            flash('You cannot unfollow yourself')
-            return redirect(url_for('user'), username = username)
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
         current_user.unfollow(user)
         db.session.commit()
-        flash('You are not following {}'.format(username))
-        return redirect(url_for('user'), username=username)
+        flash('You are not following {}.'.format(username))
+        return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
-
