8ce6a235f3d949a04388afe306bfbadddde6a263
Marta Zaguła <mzagula1992@gmail.com>
2020-09-27 00:44:30 +0200

    add email app, route and form

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,8 @@ from app.models import User, Post
 from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
 from datetime import datetime
+from app.forms import ResetPasswordRequestForm
+from app.email import send_password_reset_email
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -162,3 +164,15 @@ def explore():
                            next_url=next_url, prev_url=prev_url)
 
 
+@app.route('/reset_password_request', methods=['GET','POST'])
+def reset_password_requet():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_password_reset_email(user)
+        flash('Check your email for the instructions to reset your password')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html', title='Reset Password',)
