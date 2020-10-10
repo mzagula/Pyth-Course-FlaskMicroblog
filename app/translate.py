078ae56b6ec17b6ba1a9337b8d51bb554c828783
Marta Zaguła <mzagula1992@gmail.com>
2020-10-10 14:10:46 +0200

    fix function translate

--- app/translate.py
+++ app/translate.py
@@ -4,8 +4,10 @@ from flask_babel import _
 from app import app
 
 def translate(text, source_language, dest_language):
-    r = requests.get('https://api.mymemory.translated.net/get?q={}&langpair={}|{}').format(text,
+    page = 'https://api.mymemory.translated.net/get?q={}&langpair={}|{}'.format(text, source_language,                                                                                dest_language)
+    r = requests.get(page)
     if r.status_code != 200:
-        return json.loads(r.content.decore('utf-8-sig'))
-                                                                                          dest_language)
+        return _('Error: The translation service failed!')
+
+    return json.loads(r.content.decode('utf-8-sig'))['responseData']['translatedText']
 
