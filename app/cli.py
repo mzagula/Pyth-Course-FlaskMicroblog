40428ff76b6884e17646829169d267f77dfdeb3d
Marta Zaguła <mzagula1992@gmail.com>
2020-10-11 23:39:04 +0200

    add init command to flask translate

--- app/cli.py
+++ app/cli.py
@@ -1,11 +1,23 @@
 from app import app
 import os
+import click
 
 @app.cli.group()
 
 def translate():
     """Translation and localization commands."""
     pass
+
+
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initialize new language"""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('init command failed')
+    os.remove('messages.pot')
 
 @translate.command()
 def update():
