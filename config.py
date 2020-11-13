1e92a6bd1966a9659098b55d50d2a1845569b9b0
Marta Zaguła <mzagula1992@gmail.com>
2020-11-14 00:01:00 +0100

    heroku deploymnent changes

--- config.py
+++ config.py
@@ -17,3 +17,4 @@ class Config(object):
     POSTS_PER_PAGE = 3
     LANGUAGES = ['en', 'pl']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
